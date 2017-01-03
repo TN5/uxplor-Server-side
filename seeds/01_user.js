af17eb76be1e17fa55701e05c33752ba262fffb6
ZacJocelyn <zacjocelyn@gmail.com>
2017-01-03 13:41:13 -0700

    :tomato: created seed for feedback table, fixed es6 issue in seeds, added onDelete('cascade') to foreign keys in migrations

--- seeds/01_user.js
+++ seeds/01_user.js
@@ -2,7 +2,7 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex.raw('DELETE FROM "user"; ALTER SEQUENCE user_id_seq RESTART WITH 3;')
     .then(function () {
-      const users = [{
+      var users = [{
         id: 1,
         name: 'guest',
         email: 'guest@gmail.com',
